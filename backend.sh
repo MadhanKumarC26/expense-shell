f9dda6bbc6fa49209b5e6d8f3d8cec79a13f0298
MadhanKumarC <madhankumarc26@gmail.com>
2025-06-29 22:17:29 +0530

    new

--- backend.sh
+++ backend.sh
@@ -1,76 +1,84 @@
-source common.sh
+#!/bin/bash
 
-print_head "diable  deabult node js"
-dnf module disable nodejs -y &>>{log}
-status_check
+USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+echo "Please enter DB password:"
+read -s mysql_root_password
 
-print_head "diable  deabult node js"
-dnf module enable nodejs:20 -y &>>{log}
-status_check
+VALIDATE(){
+   if [ $1 -ne 0 ]
+   then
+        echo -e "$2...$R FAILURE $N"
+        exit 1
+    else
+        echo -e "$2...$G SUCCESS $N"
+    fi
+}
 
+if [ $USERID -ne 0 ]
+then
+    echo "Please run this script with root access."
+    exit 1 # manually exit if error comes.
+else
+    echo "You are super user."
+fi
 
+dnf module disable nodejs -y &>>$LOGFILE
+VALIDATE $? "Disabling default nodejs"
 
-print_head "Install nodejs"
-dnf install nodejs -y &>>${LOG}
-status_check
+dnf module enable nodejs:20 -y &>>$LOGFILE
+VALIDATE $? "Enabling nodejs:20 version"
 
+dnf install nodejs -y &>>$LOGFILE
+VALIDATE $? "Installing nodejs"
 
+id expense &>>$LOGFILE
+if [ $? -ne 0 ]
+then
+    useradd expense &>>$LOGFILE
+    VALIDATE $? "Creating expense user"
+else
+    echo -e "Expense user already created...$Y SKIPPING $N"
+fi
 
-#   print_head "Add Application User"
-#   id Expense &>>${LOG}
-#   if [ $? -ne 0 ]; then
-#     useradd Expense &>>${LOG}
-#   fi
-#   status_check
+mkdir -p /app &>>$LOGFILE
+VALIDATE $? "Creating app directory"
 
+curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip &>>$LOGFILE
+VALIDATE $? "Downloading backend code"
 
-print_head "add  app"
-mkdir -p /app  &>>${LOG}
-status_check
+cd /app
+rm -rf /app/*
+unzip /tmp/backend.zip &>>$LOGFILE
+VALIDATE $? "Extracted backend code"
 
+npm install &>>$LOGFILE
+VALIDATE $? "Installing nodejs dependencies"
 
-print_head "download new  content "
-curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip &>>${LOG}
-status_check
- 
+#check your repo and path
+cp /home/ec2-user/expense-shell/backend.service /etc/systemd/system/backend.service &>>$LOGFILE
+VALIDATE $? "Copied backend service"
 
-print_head "change  app"
-cd /app &>>${LOG}
-status_check 
+systemctl daemon-reload &>>$LOGFILE
+VALIDATE $? "Daemon Reload"
 
-rm -rf /app/* 
+systemctl start backend &>>$LOGFILE
+VALIDATE $? "Starting backend"
 
-print_head "unzip content"
-unzip /tmp/backend.zip &>>${LOG}
-status_check 
+systemctl enable backend &>>$LOGFILE
+VALIDATE $? "Enabling backend"
 
-npm install
+dnf install mysql -y &>>$LOGFILE
+VALIDATE $? "Installing MySQL Client"
 
+mysql -h db.devops26.shop -uroot -p${mysql_root_password} < /app/schema/backend.sql &>>$LOGFILE
+VALIDATE $? "Schema loading"
 
-cp ${script_location}/files/backend.service /etc/systemd/system/backend.service
-
-
-
-
-print_head "deamon reload"
-systemctl daemon-reload  &>>${LOG}
-status_check
-
-
-
-print_head "Start service"
-systemctl start backend &>>${LOG}
-status_check
-
-print_head "Enable service"
-systemctl enable backend &>>${LOG}
-status_check
-
-
-dnf install mysql -y &>>${LOG}
-
-
-mysql -h db.devops26.shop -uroot -p${mysql_root_password} < /app/schema/backend.sql &>>${LOG}
-
-
-systemctl restart backend &>>${LOG}
+systemctl restart backend &>>$LOGFILE
+VALIDATE $? "Restarting Backend"
--- frontend.sh
+++ frontend.sh
@@ -1,42 +1,54 @@
-source common.sh
+#!/bin/bash
 
-print_head "Install Nginx"
-dnf install nginx -y &>>${LOG}
-status_check
+USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
 
+VALIDATE(){
+   if [ $1 -ne 0 ]
+   then
+        echo -e "$2...$R FAILURE $N"
+        exit 1
+    else
+        echo -e "$2...$G SUCCESS $N"
+    fi
+}
 
-print_head "Install Nginx"
-systemctl enable nginx &>>${LOG}
-status_check
+if [ $USERID -ne 0 ]
+then
+    echo "Please run this script with root access."
+    exit 1 # manually exit if error comes.
+else
+    echo "You are super user."
+fi
 
-print_head "start Nginx"
-systemctl start nginx &>>${LOG}
-status_check
+dnf install nginx -y &>>$LOGFILE
+VALIDATE $? "Installing nginx"
 
+systemctl enable nginx &>>$LOGFILE
+VALIDATE $? "Enabling nginx"
 
-print_head "remove old Nginx content "
-rm -rf /usr/share/nginx/html/* &>>${LOG}
-status_check
+systemctl start nginx &>>$LOGFILE
+VALIDATE $? "Starting nginx"
 
-print_head "download new Nginx content "
-curl -o /tmp/frontend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-frontend-v2.zip &>>${LOG}
-status_check
+rm -rf /usr/share/nginx/html/* &>>$LOGFILE
+VALIDATE $? "Removing existing content"
 
-cd /usr/share/nginx/html &>>${LOG}
+curl -o /tmp/frontend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-frontend-v2.zip &>>$LOGFILE
+VALIDATE $? "Downloading frontend code"
 
-print_head "Extract Frontend Content"
-unzip /tmp/frontend.zip &>>${LOG}
-status_check
+cd /usr/share/nginx/html &>>$LOGFILE
+unzip /tmp/frontend.zip &>>$LOGFILE
+VALIDATE $? "Extracting frontend code"
 
+#check your repo and path
+cp /home/ec2-user/expense-shell/expense.conf /etc/nginx/default.d/expense.conf &>>$LOGFILE
+VALIDATE $? "Copied expense conf"
 
-print_head " cp new  service file Frontend Content"
-cp ${script_location}/files/expense.conf /etc/nginx/default.d/expense.conf &>>${LOG}
-status_check
-
-print_head "Enable Nginx"
-systemctl enable nginx &>>${LOG}
-status_check
-
-print_head "Start Nginx"
-systemctl restart nginx &>>${LOG}
-status_check
+systemctl restart nginx &>>$LOGFILE
+VALIDATE $? "Restarting nginx"
